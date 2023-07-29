a3cd78f790a7ae074d0575911cdb8426fe0bcee8
bregsiaju <bregsiaju@gmail.com>
2023-07-29 10:39:58 +0700

    menambahkan dots control slider

--- components/homepage/SliderCategory.vue
+++ components/homepage/SliderCategory.vue
@@ -3,11 +3,17 @@
     :slides-per-view="'auto'"
     :free-mode="true"
     :space-between="5"
-    :pagination="{ clickable: true }"
+    :grab-cursor="true"
+    :pagination="{ el: '.slider-pagination', clickable: true }"
+    :modules="[SwiperFreeMode, SwiperPagination]"
   >
     <SwiperSlide v-for="data in dataSpending" :key="data">
       <HomepageCategory :data-category="data" />
     </SwiperSlide>
+
+    <div class="slider-control">
+      <div class="slider-pagination"></div>
+    </div>
   </Swiper>
 </template>
 
@@ -36,10 +42,49 @@ const dataSpending = [
 
 <style lang="scss" scoped>
 .swiper {
-  margin-right: -22px;
+  // margin-right: -22px;
 
   .swiper-slide {
     width: auto !important;
+  }
+}
+
+.slider-control {
+  position: relative;
+  z-index: 12;
+  padding-top: 12px;
+
+  .slider-pagination {
+    text-align: center;
+
+    :deep(.swiper-pagination-bullet) {
+      width: 15px;
+      height: 5px;
+      display: inline-block;
+      // background: #dad9d9;
+      margin: 0 3px;
+      -webkit-border-radius: 6px;
+      border-radius: 6px;
+      -webkit-transition: opacity 0.5s, background-color 0.5s, width 0.5s;
+      -o-transition: opacity 0.5s, background-color 0.5s, width 0.5s;
+      transition: opacity 0.5s, background-color 0.5s, width 0.5s;
+      -webkit-transition-delay: 0.5s, 0.5s, 0s;
+      -o-transition-delay: 0.5s, 0.5s, 0s;
+      transition-delay: 0.5s, 0.5s, 0s;
+    }
+  }
+
+  :deep(.swiper-pagination-clickable .swiper-pagination-bullet) {
+    cursor: pointer;
+  }
+
+  .slider-pagination :deep(.swiper-pagination-bullet-active) {
+    opacity: 1;
+    background: $primary600;
+    width: 31px;
+    -webkit-transition-delay: 0s;
+    -o-transition-delay: 0s;
+    transition-delay: 0s;
   }
 }
 </style>
