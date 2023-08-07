e82db1f9a0a57e905423d5c8fdbe64fbca89d4e1
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-08-07 18:44:19 +0700

    added HeaderProfile as header component in profile and add header as layout in profile

---
+++ components/HeaderProfile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="bg-[#FFFD00] h-[100px] p-[10px] gap-[25px] flex flex-col justify-center"
+  >
+    <div class="flex items-center w-[23vw]">
+      <NuxtLink to="/">
+        <img src="~assets/icons/back-arrow.svg" width="40" />
+      </NuxtLink>
+      <slot name="title">
+        <h3 class="font-bold text-lg mb-1 mx-auto">Profile</h3>
+      </slot>
+    </div>
+  </div>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
