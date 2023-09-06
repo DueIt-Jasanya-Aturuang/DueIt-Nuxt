ba5bab4d411ffcc6689f0b4bb9e61cc85dcbbd40
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-09-06 19:41:38 +0700

    added delete icon and popup in kategori

--- pages/profile/kategori.vue
+++ pages/profile/kategori.vue
@@ -4,18 +4,52 @@
       <template #page-title>
         <span class="block text-center"> Kategori </span>
       </template>
-      <main class="flex flex-col bg-white gap-[219px]">
+      <main class="flex flex-col bg-white gap-[219px] relative">
         <div class="flex flex-col gap-3">
           <div
             v-for="menu in menuKategori"
             :key="menu.id"
-            class="flex gap-4 p-3 border-b shadow-sm"
+            class="flex justify-between p-3 border-b shadow-sm"
           >
-            <img :src="menu.src" :alt="menu.alt" />
-            <p>{{ menu.title }}</p>
+            <div class="flex gap-3">
+              <img :src="menu.src" :alt="menu.alt" />
+              <p>{{ menu.title }}</p>
+            </div>
+            <div class="cursor-pointer">
+              <button @click="showModal">
+                <Icon
+                  name="ant-design:delete-outlined"
+                  class="opacity-50 hover:opacity-100"
+                />
+              </button>
+            </div>
+          </div>
+          <div
+            v-show="modalVisible"
+            class="absolute bg-white bottom-0 w-full h-full"
+          >
+            <div class="flex flex-col justify-center items-center gap-4">
+              <img src="/assets/img/bro.svg" alt="ask-image" />
+              <h1>Anda Yakin ingin menghapusnya</h1>
+              <div class="flex gap-4">
+                <button
+                  class="bg-[#fef9ce] rounded-md hover:bg-[#e3d909] w-[100px] px-3 py-1 text-center"
+                  @click="hideModal"
+                >
+                  Tidak
+                </button>
+                <button
+                  class="bg-[#fef9ce] rounded-md hover:bg-[#e3d909] w-[100px] px-3 py-1 text-center"
+                >
+                  Ya
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        <ButtonsCommon class="w-full h-[40px]">Submit</ButtonsCommon>
+        <ButtonsCommon class="w-full h-[40px]"
+          >Tambah Kategori Baru</ButtonsCommon
+        >
       </main>
     </HeaderProfile>
   </section>
@@ -27,6 +61,16 @@ definePageMeta({
   // set ke layout profilePage
   layout: 'profile-page',
 })
+
+const modalVisible = ref(false)
+
+const showModal = () => {
+  modalVisible.value = true
+}
+
+const hideModal = () => {
+  modalVisible.value = false
+}
 
 const menuKategori = [
   {
